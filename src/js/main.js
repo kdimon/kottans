ed4bd2a7d414fbabca340d10eee818ea3bd3b9e5
Dmitriy <dimkakul@gmail.com>
2017-08-23 00:24:30 +0300

    Added AJAX request. Outputted required repo fields

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,68 @@ let submit = document.getElementById('submit');
 
 submit.addEventListener('click', function() {
 	let username = document.getElementById('username').value;
-	loadData();
+
+	if(!username.length) {
+	    return alert('Please, enter user/organization name!');
+    }
+
+    let body = document.body;
+    let repoSections = document.getElementsByClassName('repos');
+
+    // @todo Remove existing markup of section
+	if(repoSections.length) {
+        repoSections.remove();
+    }
+
+    let url = `https://api.github.com/users/${username}/repos`;
+
+    getHttp(url)
+        .then(
+            response => {
+                console.log(response);
+                let repo = JSON.parse(response);
+                return repo;
+            },
+            error => console.log(`Rejected: ${error}`)
+        )
+        .then(
+            repo => {
+                console.log(repo);
+                repo.forEach(function(item) {
+                    // @todo Add 'forked from'
+                    let markup = `<section class="repos">
+                                      <div>${item.name}</div>
+                                      <div>${item.description}</div>
+                                      <div>${item.stargazers_count}</div>
+                                      <div>${item.updated_at}</div>
+                                      <div>${item.language}</div>
+                                  </section>`;
+                    body.insertAdjacentHTML('beforeEnd', markup);
+
+                });
+            }
+        );
 });
 
-function loadData() {
-	let xhr = new XMLHttpRequest();
+function getHttp(url) {
+    return new Promise(function(resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
 
-	xhr.open('GET', 'https://api.github.com/users/kdimon', true);
-	xhr.send();
+        xhr.onload = function() {
+            if(this.status == 200) {
+                resolve(this.response);
+            }else {
+                let error = new Error(this.statusText);
+                error.code = this.status;
+                reject(error);
+            }
+        };
 
-	if(xhr.status != 200) {
-		console.log('Error '+ xhr.status + ': ' + xhr.statusText);
-	}else {
-		console.log(xhr.responseText);
-	}
+        xhr.onerror = function() {
+            reject(new Error('Network error'));
+        };
+
+        xhr.send();
+    });
 }
